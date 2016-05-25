309311dd758fa090ad56fa5dae3642da41d0103c
dima <klepov777@gmail.com>
2016-05-25 23:53:04 +0300

    Initial commit

---
+++ src/main/java/com/github/pires/obd/reader/activity/InjectView.java
@@ -0,0 +1,7 @@
+package com.github.pires.obd.reader.activity;
+
+/**
+ * Created by Иван on 29.03.16.
+ */
+public @interface InjectView {
+}
---
+++ src/main/java/com/github/pires/obd/reader/activity/MainActivity.java
@@ -0,0 +1,481 @@
+package com.github.pires.obd.reader.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.pires.obd.commands.ObdCommand;
+import com.github.pires.obd.commands.SpeedCommand;
+import com.github.pires.obd.commands.engine.LoadCommand;
+import com.github.pires.obd.commands.engine.RPMCommand;
+import com.github.pires.obd.commands.fuel.AirFuelRatioCommand;
+import com.github.pires.obd.enums.AvailableCommandNames;
+import com.github.pires.obd.reader.R;
+import com.github.pires.obd.reader.config.ObdConfig;
+import com.github.pires.obd.reader.io.AbstractGatewayService;
+import com.github.pires.obd.reader.io.MockObdGatewayService;
+import com.github.pires.obd.reader.io.ObdCommandJob;
+import com.github.pires.obd.reader.io.ObdGatewayService;
+import com.github.pires.obd.reader.net.ObdReading;
+import com.google.inject.Inject;
+import com.jesusm.holocircleseekbar.lib.HoloCircleSeekBar;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import roboguice.RoboGuice;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+// Some code taken from https://github.com/barbeau/gpstest
+@ContentView(R.layout.main)
+
+public class MainActivity extends RoboActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private static final int NO_BLUETOOTH_ID = 0;
+    private static final int BLUETOOTH_DISABLED = 1;
+    private static final int START_LIVE_DATA = 2;
+    private static final int STOP_LIVE_DATA = 3;
+    private static final int SETTINGS = 4;
+    private static final int GET_DTC = 5;
+    private static final int TABLE_ROW_MARGIN = 7;
+    private static final int NO_ORIENTATION_SENSOR = 8;
+
+    private static final int SAVE_TRIP_NOT_AVAILABLE = 11;
+    private static final int REQUEST_ENABLE_BT = 1234;
+    private static boolean bluetoothDefaultIsEnable = false;
+
+    static {
+        RoboGuice.setUseAnnotationDatabases(false);
+    }
+
+    public Map<String, String> commandResult = new HashMap<String, String>();
+
+    @InjectView(R.id.picker)
+    private HoloCircleSeekBar pikerRPM;
+    @InjectView(R.id.pikerSpeed)
+    private HoloCircleSeekBar pikerSpeed;
+    @InjectView(R.id.pikerRpmLoad)
+    private HoloCircleSeekBar pikerRpmLoad;
+    @InjectView(R.id.pikerThrot)
+    private HoloCircleSeekBar pikerThrot;
+    @InjectView(R.id.pikerTemp)
+    private HoloCircleSeekBar pikerTemp;
+    @InjectView(R.id.MAF)
+    private TextView MAF;
+    @InjectView(R.id.air_flue)
+    private TextView airFlue;
+    @InjectView(R.id.runtime)
+    private TextView runtime;
+
+
+
+    @Inject
+    private SensorManager sensorManager;
+    @Inject
+    private PowerManager powerManager;
+    @Inject
+    private SharedPreferences prefs;
+    private boolean isServiceBound;
+    private AbstractGatewayService service;
+    private final Runnable mQueueCommands = new Runnable() {
+        public void run() {
+            if (service != null && service.isRunning() && service.queueEmpty()) {
+                queueCommands();
+
+                double lat = 0;
+                double lon = 0;
+                double alt = 0;
+                final int posLen = 7;
+
+                if (prefs.getBoolean(ConfigActivity.UPLOAD_DATA_KEY, false)) {
+                    // Upload the current reading by http
+                    final String vin = prefs.getString(ConfigActivity.VEHICLE_ID_KEY, "UNDEFINED_VIN");
+                    Map<String, String> temp = new HashMap<String, String>();
+                    temp.putAll(commandResult);
+
+
+                } else if (prefs.getBoolean(ConfigActivity.ENABLE_FULL_LOGGING_KEY, false)) {
+                    // Write the current reading to CSV
+                    final String vin = prefs.getString(ConfigActivity.VEHICLE_ID_KEY, "UNDEFINED_VIN");
+                    Map<String, String> temp = new HashMap<String, String>();
+                    temp.putAll(commandResult);
+                    ObdReading reading = new ObdReading(lat, lon, alt, System.currentTimeMillis(), vin, temp);
+                }
+                commandResult.clear();
+            }
+            // run again in period defined in preferences
+            new Handler().postDelayed(mQueueCommands, ConfigActivity.getObdUpdatePeriod(prefs));
+        }
+    };
+    private Sensor orientSensor = null;
+    private PowerManager.WakeLock wakeLock = null;
+    private boolean preRequisites = true;
+    private ServiceConnection serviceConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder binder) {
+            Log.d(TAG, className.toString() + " service is bound");
+            isServiceBound = true;
+            service = ((AbstractGatewayService.AbstractGatewayServiceBinder) binder).getService();
+            service.setContext(MainActivity.this);
+            Log.d(TAG, "Starting live data");
+            try {
+                service.startService();
+                if (preRequisites)
+                    Log.d(TAG, "CONNECT!!!11111");
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failure Starting live data");
+                doUnbindService();
+            }
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        // This method is *only* called when the connection to the service is lost unexpectedly
+        // and *not* when the client unbinds (http://developer.android.com/guide/components/bound-services.html)
+        // So the isServiceBound attribute should also be set to false when we unbind from the service.
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            Log.d(TAG, className.toString() + " service is unbound");
+            isServiceBound = false;
+        }
+    };
+
+
+    public MainActivity(){
+
+    }
+
+    public static String LookUpCommand(String txt) {
+        for (AvailableCommandNames item : AvailableCommandNames.values()) {
+            if (item.getValue().equals(txt)) return item.name();
+        }
+        return txt;
+    }
+
+    public void stateUpdate(final ObdCommandJob job) {
+        final String cmdName = job.getCommand().getName();
+        String cmdResult = "";
+
+        final String cmdID = LookUpCommand(cmdName);
+        cmdResult = job.getCommand().getFormattedResult();
+Log.d("sdds",cmdID+" - "+cmdResult);
+
+
+        switch (cmdID){
+            case "ENGINE_RPM":
+                int rpm = ((RPMCommand) job.getCommand()).getRPM();
+                pikerRPM.setValue((float) rpm);
+                break;
+
+            case "ENGINE_LOAD":
+                float load = ((LoadCommand) job.getCommand()).getPercentage();
+                pikerRpmLoad.setValue(load);
+                break;
+
+            case "SPEED":
+                int speed = ((SpeedCommand) job.getCommand()).getMetricSpeed();
+                Log.d("kek", "spped - " + (float) speed);
+                if(speed == 0.0 ){
+                    pikerSpeed.setValue( 0);
+                }
+                pikerSpeed.setValue(speed);
+                break;
+            case "AIR_FUEL_RATIO":
+                String air = ((AirFuelRatioCommand) job.getCommand()).getFormattedResult();
+                airFlue.setText(air);
+                break;
+            case "ENGINE_COOLANT_TEMP":
+                String temp = job.getCommand().getFormattedResult().split("C")[0];
+                pikerTemp.setValue(Float.parseFloat(temp));
+                Log.d("sd", "sad");
+                break;
+
+            case "MAF":
+                String mafString = job.getCommand().getFormattedResult();
+                MAF.setText(mafString);
+                break;
+            case "THROTTLE_POS":
+                String throttle = job.getCommand().getFormattedResult().split("%")[0];
+                pikerThrot.setValue(Float.parseFloat(throttle.split(",")[0]));
+                break;
+            case "ENGINE_RUNTIME":
+                String runtimePos = job.getCommand().getFormattedResult();
+                runtime.setText(runtimePos);
+                break;
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter != null)
+            bluetoothDefaultIsEnable = btAdapter.isEnabled();
+
+
+        // get Orientation sensor
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ORIENTATION);
+        if (sensors.size() > 0)
+            orientSensor = sensors.get(0);
+        else
+            showDialog(NO_ORIENTATION_SENSOR);
+
+        int MAX_RPM = 6600;
+        int MAX_SPEED = 100;
+        pikerRPM.setMax(MAX_RPM);
+        pikerSpeed.setMax(MAX_SPEED);
+        pikerSpeed.setValue(0);
+        pikerTemp.setMax(180);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Entered onStart...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        releaseWakeLockIfHeld();
+        if (isServiceBound) {
+            doUnbindService();
+        }
+
+        final BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter != null && btAdapter.isEnabled() && !bluetoothDefaultIsEnable)
+            btAdapter.disable();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Pausing..");
+        releaseWakeLockIfHeld();
+    }
+
+    /**
+     * If lock is held, release. Lock will be held when the service is running.
+     */
+    private void releaseWakeLockIfHeld() {
+        if (wakeLock.isHeld())
+            wakeLock.release();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Resuming..");
+        wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK,
+                "ObdReader");
+
+        // get Bluetooth device
+        final BluetoothAdapter btAdapter = BluetoothAdapter
+                .getDefaultAdapter();
+
+        preRequisites = btAdapter != null && btAdapter.isEnabled();
+        if (!preRequisites && prefs.getBoolean(ConfigActivity.ENABLE_BT_KEY, false)) {
+            preRequisites = btAdapter != null && btAdapter.enable();
+        }
+
+    }
+
+    private void updateConfig() {
+        startActivity(new Intent(this, ConfigActivity.class));
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, START_LIVE_DATA, 0, getString(R.string.menu_start_live_data));
+        menu.add(0, STOP_LIVE_DATA, 0, getString(R.string.menu_stop_live_data));
+        menu.add(0, GET_DTC, 0, getString(R.string.menu_get_dtc));
+        menu.add(0, SETTINGS, 0, getString(R.string.menu_settings));
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case START_LIVE_DATA:
+                startLiveData();
+                return true;
+            case STOP_LIVE_DATA:
+                stopLiveData();
+                return true;
+            case SETTINGS:
+                updateConfig();
+                return true;
+            case GET_DTC:
+                getTroubleCodes();
+                return true;
+        }
+        return false;
+    }
+
+    private void getTroubleCodes() {
+        startActivity(new Intent(this, TroubleCodesActivity.class));
+    }
+
+    private void startLiveData() {
+        Log.d(TAG, "Starting live data..");
+
+        doBindService();
+
+
+        // start command execution
+        new Handler().post(mQueueCommands);
+        // screen won't turn off until wakeLock.release()
+        wakeLock.acquire();
+
+    }
+
+    private void stopLiveData() {
+        Log.d(TAG, "Stopping live data..");
+
+
+        doUnbindService();
+        releaseWakeLockIfHeld();
+
+        final String devemail = prefs.getString(ConfigActivity.DEV_EMAIL_KEY, null);
+        if (devemail != null) {
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case DialogInterface.BUTTON_POSITIVE:
+                            ObdGatewayService.saveLogcatToFile(getApplicationContext(), devemail);
+                            break;
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            //No button clicked
+                            break;
+                    }
+                }
+            };
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Where there issues?\nThen please send us the logs.\nSend Logs?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+        }
+
+
+    }
+
+
+    protected Dialog onCreateDialog(int id) {
+        AlertDialog.Builder build = new AlertDialog.Builder(this);
+        switch (id) {
+            case NO_BLUETOOTH_ID:
+                build.setMessage(getString(R.string.text_no_bluetooth_id));
+                return build.create();
+            case BLUETOOTH_DISABLED:
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                return build.create();
+            case NO_ORIENTATION_SENSOR:
+                build.setMessage(getString(R.string.text_no_orientation_sensor));
+                return build.create();
+        }
+        return null;
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem startItem = menu.findItem(START_LIVE_DATA);
+        MenuItem stopItem = menu.findItem(STOP_LIVE_DATA);
+        MenuItem settingsItem = menu.findItem(SETTINGS);
+        MenuItem getDTCItem = menu.findItem(GET_DTC);
+
+        if (service != null && service.isRunning()) {
+            getDTCItem.setEnabled(false);
+            startItem.setEnabled(false);
+            stopItem.setEnabled(true);
+            settingsItem.setEnabled(false);
+        } else {
+            getDTCItem.setEnabled(true);
+            stopItem.setEnabled(false);
+            startItem.setEnabled(true);
+            settingsItem.setEnabled(true);
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     */
+    private void queueCommands() {
+        if (isServiceBound) {
+            for (ObdCommand Command : ObdConfig.getCommands()) {
+                if (prefs.getBoolean(Command.getName(), true))
+                    service.queueJob(new ObdCommandJob(Command));
+            }
+        }
+    }
+
+    private void doBindService() {
+        if (!isServiceBound) {
+            Log.d(TAG, "Binding OBD service..");
+            if (preRequisites) {
+                Intent serviceIntent = new Intent(this, ObdGatewayService.class);
+                bindService(serviceIntent, serviceConn, Context.BIND_AUTO_CREATE);
+            } else {
+                Intent serviceIntent = new Intent(this, MockObdGatewayService.class);
+                bindService(serviceIntent, serviceConn, Context.BIND_AUTO_CREATE);
+            }
+        }
+    }
+
+    private void doUnbindService() {
+        if (isServiceBound) {
+            if (service.isRunning()) {
+                service.stopService();
+                if (preRequisites)
+                    Log.d(TAG,getString(R.string.status_bluetooth_ok));
+            }
+            Log.d(TAG, "Unbinding OBD service..");
+            unbindService(serviceConn);
+            isServiceBound = false;
+        }
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+
+            } else {
+                Toast.makeText(this, R.string.text_bluetooth_disabled, Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+}
